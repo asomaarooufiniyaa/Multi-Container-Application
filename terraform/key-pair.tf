8c0e218fd5af8d4c07028f0b70eb1b40b4d9505c
Aso Maaroufiniyaa <162102797+asomaarooufiniyaa@users.noreply.github.com>
2025-10-21 13:26:22 +0330

    key

--- terraform/key-pair.tf
+++ terraform/key-pair.tf
@@ -1,9 +1,5 @@
 resource "aws_key_pair" "main_key" {
-<<<<<<< HEAD
   key_name   = "main_keyyy"
-=======
-  key_name   = "main_keyy"
->>>>>>> d918f442fd9e0dbada207bcbc610bf4c2e70bdf5
   public_key = file("${path.module}/keys/id_ed25519.pub")
 
   lifecycle {
