4410c5c35fbb9a115ff9cfc6ed1fd76456349bbf
Aso Maaroufiniyaa <162102797+asomaarooufiniyaa@users.noreply.github.com>
2025-10-21 04:39:10 +0330

    key pair updated

--- terraform/key-pair.tf
+++ terraform/key-pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "main_key" {
   key_name   = "main_key"
-  public_key = file("/home/officer/.ssh/id_ed25519.pub")
+  public_key = file("${path.module}/keys/id_ed25519.pub")
 }
